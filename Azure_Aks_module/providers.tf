3ba38adcabe1e3d358ff131734ce3a1762765181
Sumit Ghosh <neo2sumitghosh@gmail.com>
2025-03-08 07:17:01 +0530

    updating nos of node in cluster

--- Azure_Aks_module/providers.tf
+++ Azure_Aks_module/providers.tf
@@ -21,9 +21,5 @@
 }
 
 provider "azurerm" {
-  subscription_id = "f5c16230-d333-4eb7-ac4c-ca790ecf432f"
-  tenant_id = "10b2fe50-512e-4a87-b279-0c4c2db1f0dc"
-  client_id = "d809d8a5-0d32-4743-98d5-47a512a69ad1"
-  client_secret = "-k08Q~3lVBIDZ3fPr7VLGuPdpIFttb0TZlvMAamt"
   features {}
 }
